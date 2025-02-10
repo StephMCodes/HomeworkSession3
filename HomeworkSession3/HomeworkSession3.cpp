c19673330cc444edc4c428e7103b9b6757b88c04
Stephanie M <shelovesgames965@gmail.com>
2025-02-10 18:12:13 -0500

    homework 10/02/25
    input and output and struct and loops

--- HomeworkSession3/HomeworkSession3.cpp
+++ HomeworkSession3/HomeworkSession3.cpp
@@ -2,10 +2,69 @@
 //
 
 #include <iostream>
+struct Course
+{
+    std::string name;
+    float midterm;
+    float exam;
+    float project;
+};
 
+bool IsInRange(int num)
+{
+    return (num >= 0 && num <= 100);
+    //this checks if num is between 0 to 100 and returns true if so
+}
+
+void CourseToString(Course& course)
+{
+    std::cout << "\nCourse: " << course.name << " Midterm: " << course.midterm << " Exam: " << course.exam << " Project: " << course.project << "\n";
+}
+
+void InputCourseInfo(Course& course, int courseNumber) {
+    std::cout << "Enter the name of the course " << courseNumber << ": ";
+    std::cin >> course.name;
+    std::cin.ignore();
+    std::cout << "Enter the midterm grade of the course " << courseNumber << ": ";
+    std::cin >> course.midterm;
+    std::cin.ignore();
+    while (!IsInRange(course.midterm)) {
+        std::cout << "Must be an integer between 0 and 100. Please try again: ";
+        std::cin >> course.midterm;
+        std::cin.ignore();
+    }
+
+    std::cout << "Enter the project grade of the course " << courseNumber << ": ";
+    std::cin >> course.project;
+    while (!IsInRange(course.project)) {
+        std::cout << "Must be an integer between 0 and 100. Please try again: ";
+        std::cin >> course.project;
+        std::cin.ignore();
+    }
+
+    std::cout << "Enter the final exam grade of the course " << courseNumber << ": ";
+    std::cin >> course.exam;
+    std::cin.ignore();
+
+    while (!IsInRange(course.exam)) {
+        std::cout << "Must be an integer between 0 and 100. Please try again: ";
+        std::cin >> course.exam;
+        std::cin.ignore();
+    }
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World! This is my C++ classwork!\n";
+    const int nbUserCourses = 3;
+    Course userCourses[nbUserCourses];
+    //loop for each possible course
+    for (int i = 0; i < nbUserCourses; i++)
+    {
+        //get input of courses
+        InputCourseInfo(userCourses[i], i + 1);
+        //display courses
+        CourseToString(userCourses[i]);
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
